8f2ed6a23c9e1e7f7b3773f4502fdb0b5d5a05e4
angelorange <acostalarangeira@gmail.com>
2019-10-31 17:48:21 -0300

    add root-page

---
+++ spec/features/visit_home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "visitor visit homepage" do
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_content('welcome to somecomix')
+    expect(page).to have_current_path(root_path)
+  end
+end
