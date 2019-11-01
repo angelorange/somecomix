a141688b458bf7b0f6c7021173ba80c84706cd98
angelorange <acostalarangeira@gmail.com>
2019-11-01 14:14:15 -0300

    add crud

---
+++ spec/features/crud_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+feature 'CRUD of comics' do
+  scenario '#index and #show successfully ' do
+    comic = create(:comic)
+
+    visit root_path
+    click on 'Comics'
+    click_on comic.title
+
+    expect(page).to have_content(comic.title)
+  end
+
+  scenario '#new and #create successfully' do
+    comic = build(:comic)
+
+    visit root_path
+    click_on 'Comics'
+    click_on 'New Comic'
+    fill_in 'Title', with: comic.title
+    click_on 'Send'
+
+    expect(page).to have_content('It was created successfully')
+    expect(page).to have_content(comic.title)
+  end
+
+  scenario '#new and #create failed' do
+    comic = build(:comic, title: '')
+
+    visit root_path
+    click_on 'Comics'
+    click_on 'New Comic'
+    fill_in 'Title', with: comic.title
+    click_on 'Send'
+
+    expect(page).not_to have_content('It was created successfully')
+    expect(page).to have_content("can't be blank")
+  end
+
+  scenario '#edit and #update successfully' do
+    comic = create(:comic)
+
+    visit root_path
+    click_on 'Comics'
+    click_on comic.title
+    click_on 'Edit'
+    fill_in 'Title', with: 'puzzlesline'
+    click_on 'Send'
+
+    expect(page).to have_content('It was succesfully edited')
+    expect(page).not_to have_content(comic.title)
+    expect(page).to have_content('puzzlesline')
+  end
+end
+
