c2fc6ed2f974829fcf6972da615af3bc30d0835f
angelorange <acostalarangeira@gmail.com>
2019-11-08 18:57:32 -0300

    add #new and #create

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,26 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(params[:id])
   end
+
+  def new
+    @comic = Comic.find(params[:comic_id])
+    @page = Page.new
+  end
+
+  def create
+    @comic = Comic.find(params[:comic_id])
+    @page = Page.new(page_params)
+      if @page.save
+        flash[:success] = 'It was created successfully'
+        redirect_to comic_page_path(@comic, @page)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:text, :comic_id)
+  end
 end
