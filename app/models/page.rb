67a47d1850d94ca542954a13ef485bb2cb3ef07e
angelorange <acostalarangeira@gmail.com>
2019-11-17 18:01:56 -0300

    fix image in Pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,8 @@
 class Page < ApplicationRecord
   belongs_to :comic
 
-  has_attached_file :image
-
+  has_attached_file :image, styles: {
+    square: '650x450#'
+  }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
