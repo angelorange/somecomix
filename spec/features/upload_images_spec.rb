8e87fca04bc79bc1e4d7196c1fb9ef6ad2032bd9
angelorange <acostalarangeira@gmail.com>
2019-12-11 23:44:47 -0300

    [wip] upload images

--- spec/features/upload_images_spec.rb
+++ spec/features/upload_images_spec.rb
@@ -6,16 +6,15 @@ feature 'registered user upload images' do
         comic = Comic.create(title: 'quadrinho')
         pagina = Page.new(text: 'um texto')
 
-        login_as(user, scope:user)
+        login_as(user, :scope => :user)
         visit root_path
         click_on 'Comics'
         click_on comic.title
-        # click_on 'New Page'
-        save_page
+        click_on 'New Page'
         fill_in 'Text', with: pagina.text
-        attach_file("Choose File", Rails.root + "spec/fixtures/connections.png")
+        attach_file("page_image", Rails.root + "spec/fixtures/connections.png")
         click_on 'Send'
-        
+        save_page
         expect(page).to have_current_path(comic_page_path(comic.id, Page.first.id))
         expect(page).to have_content(pagina.text)
         expect(page).to have_css("img[src*='connections.png']")
